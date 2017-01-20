7778239f2777c6d0a9fa58e67282d0c70e395438
Marko Harjula <marko.harjula@gmail.com>
2017-01-20 11:35:35 +0200

    Update password.js

--- password.js
+++ password.js
@@ -13,6 +13,11 @@ function makeSalt(count){
 module.exports = function (hashString) {
 	var salt = null;
 	var hash = null;
+	/**
+	 * split password parts and set salt + hash
+	 * @param {string} hashString
+	 * @returns {undefined}
+	 */
 	var makeSplit = function(hashString) {
 		var parts = hashString.split("$");
 		if ( parts[1] === "6" && parts[2].length === 8 ) {
@@ -20,28 +25,51 @@ module.exports = function (hashString) {
 			hash = parts[3];
 		}		
 	};
+	
 	if ( hashString ) {
 		makeSplit(hashString);
 	}
+	
 	this.getHash = function () {
 		return hash;
 	};
+	
 	this.getSalt = function () {
 		return salt;
 	};
+	
+	/**
+	 * get as password string
+	 * @returns {String}
+	 */
 	this.getAsString = function () {
 		if ( salt === null || hash === null ) {
 			return null;
 		}
 		return "$6$"+salt+"$"+hash;
 	};
+	
+	/**
+	 * compare password and hash
+	 * @param {type} password
+	 * @returns {Boolean} is match
+	 */
 	this.isValid = function (password) {
 		if ( salt === null || hash === null ) {
 			return false;
 		}
-		return (b64_sha512crypt(password, salt ) === this.getAsString() );
+		return ( b64_sha512crypt(password, salt ) === this.getAsString() );
 	};
+	
+	/**
+	 * generate password hash and return current instance
+	 * new Password().create("password");
+	 * 
+	 * @param {string} password
+	 * @returns {self}
+	 */
 	this.create = function (password) {
-		makeSplit(b64_sha512crypt(password, makeSalt(8) ));
+		makeSplit( b64_sha512crypt( password , makeSalt(8) ));
+		return this;
 	};
 };
